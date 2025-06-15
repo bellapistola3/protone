0434d5544374cbbc2970c9346cb1770c2ba348e3
bellapistola3 <methodman9090@gmail.com>
2025-06-15 05:29:43 +0300

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => (
+  <div>
+    <h1>Hello, welcome to Protone!</h1>
+  </div>
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
