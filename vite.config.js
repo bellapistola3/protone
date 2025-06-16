665f6ef87b9a11ffdaf84a698df7f8a0057fd213
Hristo DIMOV <167522989+bellapistola3@users.noreply.github.com>
2025-06-16 04:36:27 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  root: './src', // 🟢 това е важно!
   plugins: [react()],
-});
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true
+  }
+})
